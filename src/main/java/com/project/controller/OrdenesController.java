0105b5c0e2b09d7b3bcc3b5557fcacea586111ed
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-10-08 22:18:56 -0500

    add solution CRUD Order

--- src/main/java/com/project/controller/OrdenesController.java
+++ src/main/java/com/project/controller/OrdenesController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class OrdenesController {
@@ -17,13 +20,36 @@ public class OrdenesController {
     @GetMapping("/ordenes")
     public String obtenerOrdenes(Model model) {
         model.addAttribute("ordenes", serviceOrden.listarOrdenes());
+        model.addAttribute("orden", new Ordenes());
         return "ordenes";
     }
 
     @GetMapping("/agregarOrden")
     public String agregar(Model model) {
         model.addAttribute("employee", new Ordenes());
-        return "form";
+        return "ordenes";
+    }
+
+    @PostMapping("/agregarOrden")
+    public String agregarOrden(Ordenes orden) {
+        serviceOrden.guardarOrden(orden);
+        return "redirect:/ordenes";
+    }
+    @GetMapping("/editarOrden/{id}")
+    public String mostrarFormularioEdicion(@PathVariable Long id, Model model) {
+        Optional<Ordenes> optionalOrden = serviceOrden.buscarOrden(id);
+        if (optionalOrden.isPresent()) {
+            model.addAttribute("orden", optionalOrden.get());
+            return "FormOrder";
+        } else {
+            return "redirect:/ordenes";
+        }
+    }
+    @PostMapping("/editarOrden/{id}")
+    public String actualizarOrden(@PathVariable Long id, Ordenes orden) {
+        orden.setId(id);
+        serviceOrden.guardarOrden(orden);
+        return "redirect:/ordenes";
     }
     @GetMapping("/eliminarOrden/{id}")
     public String eliminarOrden(@PathVariable long id, Model model) {
